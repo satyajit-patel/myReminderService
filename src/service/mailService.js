502c3e2b0976ce0a65803d4dbc31dc67a184995f
Satyajit Patel <satyajitpatel770001@gmail.com>
2023-01-02 18:12:17 +0530

    added try catch in mailService

--- src/service/mailService.js
+++ src/service/mailService.js
@@ -1,13 +1,17 @@
 let sender = require("../config/mailConfig.js");
 
 let sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
-    let response = await sender.sendMail({
-        from    : mailFrom,
-        to      : mailTo,
-        subject : mailSubject,
-        text    : mailBody
-    });
-    console.log(response)
+    try {
+        let response = await sender.sendMail({
+            from    : mailFrom,
+            to      : mailTo,
+            subject : mailSubject,
+            text    : mailBody
+        });
+        console.log(response);
+    } catch(error) {
+        console.log(error);
+    }
 }
 
 module.exports = {
